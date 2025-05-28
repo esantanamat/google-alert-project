c52f87f8a53fefb940c613a7c69b8f111e98f322
esantanamat <esantanamat@gmail.com>
2025-05-28 15:46:43 -0400

    Format terraform lines

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,18 +1,18 @@
 
 variable "region" {
   description = "AWS resources region"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "environment" {
   description = "AWS resources environment"
-  type = string
-  default = "test"
+  type        = string
+  default     = "test"
 }
 
 variable "lambda_s3_bucket" {
   description = "Lambda s3 bucket for tf github actions"
-  type = string
-  default = "amzn-s3-dev-bucket-es12452"
+  type        = string
+  default     = "amzn-s3-dev-bucket-es12452"
 }
