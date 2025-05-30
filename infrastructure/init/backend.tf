9bbf5369ef5ce77c700dc29d98506424b89082c4
esantanamat <esantanamat@gmail.com>
2025-05-30 16:32:47 -0400

    restructuring ci/cd pipelines

--- infrastructure/init/backend.tf
+++ infrastructure/init/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "amzn-s3-dev-bucket-es12452"
-    key            = "terraform.tfstate"
+    key            = "init/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
---
+++ infrastructure/main/backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "amzn-s3-dev-bucket-es12452"
+    key            = "main/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
