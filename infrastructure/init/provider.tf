1ebdace995cddc84301cdeb66fd47321c74f7bd2
esantanamat <esantanamat@gmail.com>
2025-05-31 17:18:44 -0400

    changing provider version

--- infrastructure/init/provider.tf
+++ infrastructure/init/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 1.12.1"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.11.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
