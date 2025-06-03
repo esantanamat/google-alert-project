cb78b573f2907c6b5087dd6f9d3d6ae39d6bc033
esantanamat <esantanamat@gmail.com>
2025-06-03 11:09:52 -0400

    properly working selenium script, however not posting to dynamodb

--- testing/testing.py
+++ testing/testing.py
@@ -1,21 +1,43 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import traceback
+options = Options()
+#options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+try:
+    driver.get("")
 
-title = driver.title
 
-driver.implicitly_wait(0.5)
+    driver.find_element(By.ID, "user_id").send_keys("12345")
+    driver.find_element(By.ID, "destination_name").send_keys("Mall")
+    driver.find_element(By.ID, "arrival_time").send_keys("12:03")
+    driver.find_element(By.ID, "arrival_datetime").send_keys("2025-05-31T23:59")
+    driver.find_element(By.ID, "is_one_time_yes").click()
+    driver.find_element(By.ID, "origin_address").send_keys("")
+    driver.find_element(By.ID, "destination_address").send_keys("1400 Willowbrook Blvd, Wayne, NJ 07470")
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    driver.execute_script("document.querySelector('form').submit();")
 
-text_box.send_keys("Selenium")
-submit_button.click()
+    
+    WebDriverWait(driver, 20).until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    print("Alert text:", alert.text)
+    alert.accept()
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+except Exception as e:
+    print("Exception Occurred:", e)
+    traceback.print_exc()
+finally:
+    driver.quit()
 
-driver.quit()
+#Form does not return submit alert, and does not post to dynamodb, might be AWS anti-bot security?
