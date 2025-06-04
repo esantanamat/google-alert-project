a7452df742bc689f0ab813134920af3a4ebaed38
esantanamat <esantanamat@gmail.com>
2025-06-04 10:55:29 -0400

    finalizing the lambda function to handle time logic

--- lambda-reminder-checker/reminder-api-lambda-function.py
+++ lambda-reminder-checker/reminder-api-lambda-function.py
@@ -2,57 +2,54 @@ import boto3
 from datetime import datetime, timedelta, timezone
 import json
 
-
 dynamodb = boto3.resource('dynamodb')
-sns = boto3.resource('sns')
-
 TABLE_NAME = 'google-project-table'
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:463470969308:arriveby-alert-sns'
 
 headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        
-    }
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+}
 
-#This lambda function should get triggered by cloudwatch events
 def lambda_handler(event, context):
     table = dynamodb.Table(TABLE_NAME)
     now = datetime.now(timezone.utc)
     one_hour_later = now + timedelta(hours=1)
-    nowhformat = now.strftime('%H:%M:%S')
-    one_hour_later_hformat = one_hour_later.strftime('%H:%M:%S')
+    matches = []
 
     response = table.scan()
     for item in response['Items']:
         id = item.get('user_id')
         one_time_toggle = item.get('is_one_time')
-        if one_time_toggle == 'yes':
-            parsed_reminder_time = datetime.strptime(item.get('arrival_time'), '%H:%M').time()
-            reminder_time = now.replace(hour=parsed_reminder_time.hour,minute=parsed_reminder_time.minute,second = 0, microsecond = 0)
 
-            if not reminder_time:
-                continue
-            within_next_hour =  nowhformat <= reminder_time <= one_hour_later_hformat
-            
-            if within_next_hour:
-                return {
-                    "statusCode": 200,
-                    "headers": headers,
-                    "body": json.dumps({"message": f"the time is within the hour ${id}"})
-                }
-                #better to invoke another labmda function that handles the google api call
+        try:
+            if one_time_toggle == 'yes':
+                
+                parsed_time = datetime.strptime(item.get('arrival_time'), '%H:%M').time()
+                reminder_time = now.replace(
+                    hour=parsed_time.hour,
+                    minute=parsed_time.minute,
+                    second=0,
+                    microsecond=0
+                )
             else:
-                continue
-        else:
-            arrival_datetime_str = item.get('arrival_datetime')
-            reminder_time = datetime.strptime(arrival_datetime_str, '%Y-%m-%dT%H:%M').replace(tzinfo=timezone.utc)
+                
+                parsed_datetime = datetime.strptime(item.get('arrival_datetime'), '%Y-%m-%dT%H:%M')
+                reminder_time = parsed_datetime.replace(tzinfo=timezone.utc)
 
-            if not reminder_time:
-                continue
-            within_next_hour = now <= reminder_time <= one_hour_later
-            if within_next_hour:
-                print(f'it is within the next hour (1 time thing format) ${id} ${reminder_time}')        
-        
-       
+            if reminder_time < now:
+                reminder_time += timedelta(days=1)
+
+            if now <= reminder_time <= one_hour_later:
+                matches.append({'user_id': id, 'reminder_time': reminder_time})
+
+        except Exception as e:
+            
+            print(f"Error processing item {item}: {e}")
+            continue
+
+    return {
+        "statusCode": 200,
+        "headers": headers,
+        "body": json.dumps({"matches": matches}, default=str)
+    }
