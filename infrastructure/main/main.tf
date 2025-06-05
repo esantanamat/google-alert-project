64257028a1795af14547862640035798114b72e2
esantanamat <esantanamat@gmail.com>
2025-06-05 17:11:47 -0400

    adding stepfunctions + eventbridge every hour

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -365,93 +365,138 @@ resource "null_resource" "dummy_trigger" {
 }
 
 
-#adding eventbridge  and step function to orchestrate the lambdas
-
-# resource "aws_sfn_state_machine" "arrival_time_notifier_sm" {
-#   name     = "arrival_time_notifier_sm"
-#   role_arn = aws_iam_role.step_function_role.arn
-
-#   definition = jsonencode({
-#     StartAt = "CheckArrivalTime",
-#     States = {
-#       CheckArrivalTime = {
-#         Type       = "Task",
-#         Resource   = aws_lambda_function.reminder_api_lambda.arn,
-#         Next       = "FetchGoogleAPI",
-#         ResultPath = "$.checkResult"
-#       },
-#       FetchGoogleAPI = {
-#         Type       = "Task",
-#         Resource   = "arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:fetch_google_api", # To be updated
-#         Next       = "SendSMS",
-#         ResultPath = "$.googleResult"
-#       },
-#       SendSMS = {
-#         Type     = "Task",
-#         Resource = "arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:send_sms", # To be updated
-#         End      = true
-#       }
-#     }
-#   })
-# }
 
 
+resource "aws_sfn_state_machine" "arrival_time_notifier_sm" {
+  name     = "arrival_time_notifier_sm"
+  role_arn = aws_iam_role.step_function_role.arn
 
-# resource "aws_cloudwatch_event_rule" "every_hour" {
-#   name                = "trigger_step_function_every_hour"
-#   schedule_expression = "rate(1 hour)"
-# }
+  definition = jsonencode({
+    StartAt = "CheckArrivalTime",
+    States = {
+      CheckArrivalTime = {
+        Type       = "Task",
+        Resource   = aws_lambda_function.reminder_api_lambda.arn,
+        Next       = "FetchGoogleAPI",
+        ResultPath = "$.checkResult"
+      },
+      FetchGoogleAPI = {
+        Type       = "Task",
+        Resource   = aws_lambda_function.google_api_function.arn, 
+        Next       = "SendEmail",
+        ResultPath = "$.googleResult"
+      },
+      SendEmail = {
+        Type     = "Task",
+        Resource = aws_lambda_function.email_notification_lambda.arn, 
+        End      = true
+      }
+    }
+  })
+}
 
 
 
-# resource "aws_iam_role_policy" "eventbridge_step_function_permission" {
-#   name = "allow_eventbridge_to_start_step_function"
-#   role = aws_iam_role.step_function_role.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = "states:StartExecution",
-#         Resource = aws_sfn_state_machine.arrival_time_notifier_sm.arn
-#       }
-#     ]
-#   })
-# }
 
 
-# resource "aws_iam_role" "step_function_role" {
-#    name = "step_function_exec_role"
+resource "aws_iam_role_policy" "eventbridge_step_function_permission" {
+  name = "allow_eventbridge_to_start_step_function"
+  role = aws_iam_role.step_function_role.id
 
-#   assume_role_policy = jsonencode({
-#   Version = "2012-10-17",
-#   Statement = [
-#       {
-#       Action = "sts:AssumeRole",
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "states.amazonaws.com"
-#       }
-#   }]
-#   })
-# }
-
-# resource "aws_lambda_permission" "stepfunc_lambda_1" {
-#   statement_id  = "AllowStepFunctionInvokeReminderChecker"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.reminder_api_lambda.function_name
-#   principal     = "states.amazonaws.com"
-#   source_arn    = aws_sfn_state_machine.arrival_time_notifier_sm.arn
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "states:StartExecution",
+        Resource = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+      }
+    ]
+  })
+}
 
 
-# resource "aws_cloudwatch_event_target" "step_function_target" {
-#   rule      = aws_cloudwatch_event_rule.every_hour.name
-#   target_id = "step-function-invoker"
-#   arn       = aws_sfn_state_machine.arrival_time_notifier_sm.arn
-#   role_arn  = aws_iam_role.step_function_role.arn
-# }
+resource "aws_iam_role" "step_function_role" {
+   name = "step_function_exec_role"
+
+  assume_role_policy = jsonencode({
+  Version = "2012-10-17",
+  Statement = [
+      {
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "states.amazonaws.com"
+      }
+  }]
+  })
+}
+
+resource "aws_lambda_permission" "stepfunc_lambda_1" {
+  statement_id  = "AllowStepFunctionInvokeReminderChecker"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.reminder_api_lambda.function_name
+  principal     = "states.amazonaws.com"
+  source_arn    = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+}
+
+resource "aws_lambda_permission" "stepfunc_lambda_2" {
+  statement_id  = "AllowStepFunctionInvokeGoogleAPIFetcher"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.google_api_function.function_name
+  principal     = "states.amazonaws.com"
+  source_arn    = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+}
+
+resource "aws_lambda_permission" "stepfunc_lambda_3" {
+  statement_id  = "AllowStepFunctionInvokeEmailSender"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.email_notification_lambda.function_name
+  principal     = "states.amazonaws.com"
+  source_arn    = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+}
+
+resource "aws_cloudwatch_event_target" "step_function_target" {
+  rule      = aws_cloudwatch_event_rule.every_hour.name
+  target_id = "step-function-invoker"
+  arn       = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+  role_arn  = aws_iam_role.eventbridge_stepfn_invoker.arn
+}
+
+resource "aws_cloudwatch_event_rule" "every_hour" {
+  name                = "trigger_step_function_every_hour"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_iam_role" "eventbridge_stepfn_invoker" {
+  name = "eventbridge-stepfn-invoker"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "events.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "eventbridge_stepfn_policy" {
+  name = "eventbridge-stepfn-policy"
+  role = aws_iam_role.eventbridge_stepfn_invoker.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = "states:StartExecution",
+      Resource = aws_sfn_state_machine.arrival_time_notifier_sm.arn
+    }]
+  })
+}
+
 
 variable "api_key_secret" {
   type      = string
