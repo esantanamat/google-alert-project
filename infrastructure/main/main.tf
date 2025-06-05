7bb16d6a50046c0a049276d81269ffe3aad1cd70
esantanamat <esantanamat@gmail.com>
2025-06-05 10:27:58 -0400

    adding 3rd function, and secret for api key

--- infrastructure/main/main.tf
+++ infrastructure/main/main.tf
@@ -159,7 +159,7 @@ resource "aws_iam_role_policy_attachment" "attach_ecr_policy_reminder_api_lambda
 
 
 resource "aws_lambda_function" "my_lambda" {
-  function_name = "google-api-lambda-function"
+  function_name = "http-lambda-function"
   image_uri     = "${data.terraform_remote_state.init.outputs.ecr_repository_url}:http-handler-latest"
   package_type  = "Image"
   role          = aws_iam_role.lambda_exec.arn
@@ -173,6 +173,54 @@ resource "aws_lambda_function" "reminder_api_lambda" {
   role          = aws_iam_role.reminder_exec.arn
   timeout       = 30
 }
+
+resource "aws_lambda_function" "google_api_function" {
+  function_name = "google-api-lambda-function"
+  image_uri     = "${data.terraform_remote_state.init.outputs.ecr_repository_url}:google-api-func-latest"
+  package_type  = "Image"
+  role          = aws_iam_role.reminder_exec.arn
+  timeout       = 30
+}
+
+resource "aws_iam_role" "google_api_role" {
+  name = "google-api-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "secrets_fetch_policy" {
+  name = "secrets-fetch-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["secretsmanager:GetSecretValue"],
+      Resource = aws_secretsmanager_secret.api_key_secret.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_secrets_fetch_policy" {
+  role       = aws_iam_role.google_api_role.name
+  policy_arn = aws_iam_policy.secrets_fetch_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "google_api_lambda_basic_execution" {
+  role       = aws_iam_role.google_api_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+
 
 
 resource "aws_apigatewayv2_api" "http_api" {
